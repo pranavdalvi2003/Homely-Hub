fa39f0a52b251f7b4618b568aac62e1d9839f370
Pranav Dalvi <pranavdalvi2003@gmail.com>
2024-04-24 11:59:57 +0530

    Updated Code

--- client/src/store/actions/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./error";
-export * from "./auth";
-export * from "./faculty";
-export * from "./internships";
-export * from "./notices";
-export * from "./admin";
-export * from "./success";
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.auth = require("./auth");
-
-module.exports.internships = require("./internship");
-
-module.exports.admin = require("./admin");
-
-module.exports.notices = require("./notices");
-
-module.exports.Faculty = require("./faculty");
